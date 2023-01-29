80bd4ab03e431cb390d07c6e099e6e10eb0fff32
diopmorfall <morfall.papydiop99@gmail.com>
2023-01-29 21:38:27 +0100

    Tailwind installed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <div className="App">
-                
+                <h2 className='text-3xl text-red-700'>Multistep form</h2>
             </div>
             <div class="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
