1aead7a8d3cd53e9ec144982ba8db55a733e4d32
diopmorfall <morfall.papydiop99@gmail.com>
2023-02-02 19:55:24 +0100

    Dependencies added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
             <div className="App">
                 <h2 className='text-3xl text-red-700'>Multistep form</h2>
             </div>
-            <div class="attribution">
+            <div className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
                 Coded by <a href="#">Mor Fall Diop</a>.
             </div>
