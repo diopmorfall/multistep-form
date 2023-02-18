73358d6fe521cf62f976334ba0468526de781291
diopmorfall <morfall.papydiop99@gmail.com>
2023-02-18 22:14:21 +0100

    PickAddOns style added

--- src/components/PickAddOnsPage/PickAddOnsPage.js
+++ src/components/PickAddOnsPage/PickAddOnsPage.js
@@ -11,24 +11,33 @@ export default function PickAddOnsPage() {
     const dispatch = useDispatch();
 
     return (
-        <section className="add-ons">
+        <section className={moduleStyles["add-ons"]}>
             <h1>Pick add-ons</h1>
-            <h3>Add-ons help enhance yout gaming experience</h3>
-            <div className="options">
+            <h3>Add-ons help enhance your gaming experience</h3>
+            <div className={moduleStyles["add-ons-container"]}>
                 {addOns.map(addOn => (
                     <div
                         key={addOn.name}
-                        className={moduleStyles['plan']}
-                        style={addOn.isSelected ? { border: '2px solid purple' } : {}}
+                        className={addOn.isSelected ?
+                            `${moduleStyles["add-on"]} ${moduleStyles["selected-add-on"]}`
+                            : moduleStyles["add-on"]
+                        }
                         onClick={() =>  addOn.isSelected
                             ? dispatch(removeAddOn(addOn.name))
                             : dispatch(selectAddOn(addOn.name))
                         }
                     >
-                        <input type="checkbox" name="" id="" checked={addOn.isSelected} />
-                        <h5>{addOn.name}</h5>
-                        <p>{addOn.description}</p>
-                        <span>
+                        <input className={moduleStyles["add-on-checkbox"]}
+                            type="checkbox"
+                            name=""
+                            id=""
+                            checked={addOn.isSelected}
+                        />
+                        <div className={moduleStyles["add-on-details"]}>
+                            <h5 className={moduleStyles["add-on-name"]}>{addOn.name}</h5>
+                            <p className={moduleStyles["add-on-desc"]}>{addOn.description}</p>
+                        </div>
+                        <span className={moduleStyles["add-on-price"]}>
                             {isYearlyPayment
                                 ? `+${addOn.price.yearly}/yr`
                                 : `+${addOn.price.monthly}/mo`}
