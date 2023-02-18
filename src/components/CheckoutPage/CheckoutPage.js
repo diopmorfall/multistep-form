e65cf3d8162e251d95c6407eab2472b8a233bb54
diopmorfall <morfall.papydiop99@gmail.com>
2023-02-19 00:05:05 +0100

    CheckoutPage style added

--- src/components/CheckoutPage/CheckoutPage.js
+++ src/components/CheckoutPage/CheckoutPage.js
@@ -3,21 +3,21 @@ import moduleStyles from './CheckoutPage.module.css';
 
 export default function CheckoutPage() {
     return (
-        <>
+        <div>
             <section className={moduleStyles['checkout']}>
                 <img src="" alt="ok" />
-                <h2>Thank you!</h2>
-                <p>
+                <h1>Thank you!</h1>
+                <p className={moduleStyles["greetings"]}>
                     Thanks for confirming your subscription!
                     <br />
                     We hope you have fun using our platform. If you ever need support,
                     please feel free to email us at support@loremgaming.com
                 </p>
             </section>
-            <div className="attribution">
+            <div className={moduleStyles["attribution"]}>
                 Challenge from <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
                 Built by <a href="https://github.com/diopmorfall/">Mor Fall Diop</a>.
             </div>;
-        </>
+        </div>
     );
 }
