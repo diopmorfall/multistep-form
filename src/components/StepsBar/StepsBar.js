5b7661d1e7dd30d6ee343e6b08d03d313c8ad38c
diopmorfall <morfall.papydiop99@gmail.com>
2023-03-07 20:41:42 +0100

    Basic stepsbar styling

--- src/components/StepsBar/StepsBar.js
+++ src/components/StepsBar/StepsBar.js
@@ -4,17 +4,22 @@ import moduleStyles from './StepsBar.module.css';
 
 export default function StepsBar() {
     const page = useSelector(state => state.pages.currentPage);
+    const steps = ['Your info', 'Select plan', 'Add-ons', 'Summary'];
+
     return (
-        <nav>
+        <nav className='bg-stepsBar'>
             <ul className={moduleStyles['ul']}>
                 {[1, 2, 3, 4].map(step => (
                     <li key={step}
-                        style={page === step ?
-                            { backgroundColor: 'lightblue', border: '2px solid lightblue' } :
-                            {}
-                        }
+                        className={page === step ? moduleStyles['current-page-li'] : ''}
                     >
-                        {step}
+                        <span
+                            className={page === step ? moduleStyles['current-page'] : ''}
+                        >{step}</span>
+                        <div className={moduleStyles['caption']}>
+                            <span>STEP {step}</span>
+                            <span>{steps[step + 1]}</span>
+                        </div>
                     </li>
                 ))}
             </ul>
